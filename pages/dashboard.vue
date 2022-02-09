973bf104cee46acfd88169f83f2eedce4e26e2d5
obinna <biostechnologyng@gmail.com>
2022-02-09 10:46:08 +0100

    Update

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="container-fluid">
-<h1>Welcome</h1>
+  <div class="container-fluid p-0">
+    <v-card color="grey lighten-4" flat height="200px" tile>
+      <v-toolbar dense>
+        <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+        <v-toolbar-title>
+          <section>
+            <v-img class="overlay-img" src="getchangelogo.png"> </v-img>
+          </section>
+       </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-circle</v-icon>
+
+        </v-btn>
+<span>Hi, Joshua</span>
+        <v-btn icon>
+          <v-icon>mdi-arrow-down</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
   </div>
 </template>
+
+
+<style scoped>
+.container-fluid {
+  background-color: #e5e5e5;
+}
+</style>

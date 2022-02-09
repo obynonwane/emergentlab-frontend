67e71b87186dfd665e321a1afc5746a6fbca444f
obinna <biostechnologyng@gmail.com>
2022-02-09 06:06:09 +0100

    Heroku deploy

--- pages/index.vue
+++ pages/index.vue
@@ -19,62 +19,79 @@
           <div class="col d-flex justify-content-center">
             <div class="card">
               <div class="card-body">
-                <v-form v-model="valid"  ref="form">
+                <v-form v-model="valid" ref="form">
                   <!-- <v-container> -->
-                    <v-row>
-                      <v-col cols="12" md="6">
-                        <v-text-field
-                          append-icon="mdi-account"
-                          v-model="firstname"
-                          :rules="nameRules"
-                          label="First name"
-                          required
-                        ></v-text-field>
-                      </v-col>
+                  <v-row>
+                    <v-col cols="12" md="6">
+                      <v-text-field
+                        append-icon="mdi-account"
+                        v-model="firstname"
+                        :rules="nameRules"
+                        label="First name"
+                        required
+                      ></v-text-field>
+                    </v-col>
 
-                      <v-col cols="12" md="6">
-                        <v-text-field
-                          append-icon="mdi-account"
-                          v-model="lastname"
-                          :rules="nameRules"
-                          label="Last name"
-                          required
-                        ></v-text-field>
-                      </v-col>
+                    <v-col cols="12" md="6">
+                      <v-text-field
+                        append-icon="mdi-account"
+                        v-model="lastname"
+                        :rules="nameRules"
+                        label="Last name"
+                        required
+                      ></v-text-field>
+                    </v-col>
 
-                      <v-col cols="12" md="12">
-                        <v-text-field
-                          append-icon="mdi-email"
-                          v-model="email"
-                          :rules="emailRules"
-                          label="E-mail"
-                          required
-                        ></v-text-field>
-                      </v-col>
+                    <v-col cols="12" md="12">
+                      <v-text-field
+                        append-icon="mdi-email"
+                        v-model="email"
+                        :rules="emailRules"
+                        label="E-mail"
+                        required
+                      ></v-text-field>
+                    </v-col>
 
-                      <v-col cols="12" md="12">
-                        <v-text-field
-                          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                          :rules="[rules.required, rules.emailMatch]"
-                          :type="show1 ? 'text' : 'password'"
-                          name="input-10-2"
-                          label="Password"
-                          hint="At least 8 characters"
-                          value="Pa"
-                          error
-                          @click:append="show1 = !show1"
-                          required
-                        ></v-text-field>
-                      </v-col>
+                    <v-col cols="12" md="12">
+                      <v-text-field
+                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                        :rules="[rules.required, rules.emailMatch]"
+                        :type="show1 ? 'text' : 'password'"
+                        name="input-10-2"
+                        label="Password"
+                        hint="At least 8 characters"
+                        value="Pa"
+                        error
+                        @click:append="show1 = !show1"
+                        required
+                      ></v-text-field>
+                    </v-col>
 
-                      <section class="btn-submit d-flex justify-content-right">
-                        <button type="button" class="btn btn-success btn-inner">Success</button>
-                      </section>
-                    </v-row>
+                    <section class="btn-submit d-flex justify-content-right">
+                      <button
+                        type="button"
+                        class="btn btn-lg btn-success btn-inner"
+                      >
+                        Success
+                      </button>
+                    </section>
+                  </v-row>
                   <!-- </v-container> -->
                 </v-form>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="row bottom-section">
+          <div class="col-sm">
+            <p class="footer-test">
+              By siging up, you agree to our <span class="subtext-in">Terms</span> and <span class="subtext-in">Privacy Policy</span>
+            </p>
+          </div>
+          <div class="col-sm">
+            <p class="footer-copyright">
+              © 2019 Tinylabs. All rights reserved.
+            </p>
           </div>
         </div>
       </div>
@@ -87,7 +104,7 @@ export default {
   name: "IndexPage",
   data() {
     return {
-       valid: true,
+      valid: true,
       show1: false,
       password: "Password",
       rules: {
@@ -106,17 +123,17 @@ export default {
     };
   },
 
-   methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
+  methods: {
+    validate() {
+      this.$refs.form.validate();
     },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
 };
 </script>
 <style scoped>
@@ -164,11 +181,54 @@ export default {
 .card {
   margin-left: 120px;
   margin-right: 120px;
+  padding-bottom: 20px;
   background: #ffffff;
   box-shadow: 0px 20px 50px #eaeee9;
   border-radius: 5px;
 }
 .btn-inner {
+  text-align: center;
+  color: #ffffff;
+  background-color: #2bda53;
+  width: 50%;
+}
+
+.btn-submit {
+  display: flex;
+  justify-content: right;
+  width: 100%;
+}
+
+.footer-test {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 13px;
+  line-height: 18px;
+  /* identical to box height, or 138% */
+
+  letter-spacing: -0.08px;
+
+  color: #6a7e8a;
+}
+
+.footer-copyright {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 13px;
+  line-height: 18px;
+  /* identical to box height, or 138% */
+
   text-align: right;
+  letter-spacing: -0.08px;
+
+  color: #6a7e8a;
+}
+
+.bottom-section {
+  padding-top:80px;
+  padding-left: 30px;
+  padding-right: 30px;
 }
 </style>
